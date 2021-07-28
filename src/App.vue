3eed828c3a80141b83a57923b459652dbfc1ae7d
Oumou <oumou-niakate@live.fr>
2021-07-29 00:21:53 +0200

    add to list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <h1>My to do list</h1>
+  <form action="">
+    <input type="text" placeholder="example buy candy..." v-model="addToList">
+    <button @click.prevent="valid">Add</button>
+  </form>
+  <ul v-for="(item,index) in myTabs"
+  v-bind:key="index">
+    <li v-bind:style="{background: color, padding: margeInt, display: visibility}" @click='supp'>{{item}}</li>
+  </ul>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ data:function(){
+   return{
+     addToList: '',
+     myTabs: [],
+     color: 'pink',
+     margeInt: '10px',
+     visibility : true
+   }
+ },methods:{
+   valid:function(){
+     this.myTabs.push(this.addToList)
+    this.addToList = ''
+   },
+
+ }
 }
 </script>
 
@@ -22,5 +42,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input, button{
+  padding: 10px;
 }
 </style>
